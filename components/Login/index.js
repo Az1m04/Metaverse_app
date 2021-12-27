3e116bab37aa0d7a0ccfb2ae095af2086c2012e5
Azim <azim.babu@simbaquartz.com>
2021-12-28 02:11:21 +0530

    glitched text

--- components/Login/index.js
+++ components/Login/index.js
@@ -1,28 +1,137 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
-import {useMoralis} from "react-moralis"
-import Me from '../../public/img/azim.jpg'
+import { useMoralis } from "react-moralis";
+import Me from "../../public/img/azim.jpg";
+import BG from "../../public/img/bg.jpg";
+import Wallet from "../../public/img/wallet.svg";
+import Particles from "react-tsparticles";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Style from "./index.module.css";
 
 function Login() {
-  const {authenticate}=useMoralis()
+  const { authenticate } = useMoralis();
+  useEffect(() => {
+    AOS.init({ duration: 1600 });
+  });
   return (
-    <div className="bg-black relative ">
-      <h1>i am Login</h1>
-      <div className="flex flex-col absolute z-50 h-4/6 items-center space-y-4 justify-center w-full" >
-      <Image
-      className="object-cover rounded-full"
-          src={Me} 
-           height={200}
-           width={200}
-        />
-        <button onClick={authenticate} className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse"> <h1>Login to Metaverse</h1></button>
+    <div className="bg-black relative  ">
+      <div className="flex  flex-col absolute items-center z-50 h-screen  space-y-4 justify-center w-full">
+        <div
+          className={`${Style.glitch} uppercase wrapper text-white`}
+          style={{
+            fontSize: "160px",
+
+            fontWeight: "bolder",
+          }}
+        >
+          <p className="glitch">
+            <span aria-hidden="true">Welcome to Metaverse </span> 
+            Welcome to Metaverse
+            <span aria-hidden="true">Welcome to Metaverse </span>
+          </p>
+        </div>
+        <div div data-aos="zoom-in" className="mt-10">
+          {" "}
+          {/* <Image
+              className="object-cover  rounded-full"
+              src={Me}
+              height={200}
+              width={200}
+            /> */}
+        </div>
+        <div data-aos="fade-up" className="mt-5">
+          <button
+            onClick={authenticate}
+            className="bg-yellow-500 rounded-lg p-5 font-bold"
+          >
+            <div className="flex items-center">
+              <div className="mr-5">
+                {" "}
+                <Image src={Wallet} height={40} width={40} />
+              </div>
+              <h1 style={{ fontSize: "16px" }}>Connect your wallet</h1>
+            </div>
+          </button>
+        </div>
       </div>
-      <div className="w-full h-screen ">
-        <Image
-          src="https://links.papareact.com/55n"
-          layout="fill"
-          objectFit="cover"
-        />
+
+      <div className="w-full h-screen">
+        <Image src={BG} layout="fill" objectFit="cover" />
+        <div className="z-100">
+          {" "}
+          <Particles
+            options={{
+              interactivity: {
+                events: {
+                  onClick: {
+                    enable: true,
+                    mode: "push",
+                  },
+                  onHover: {
+                    enable: true,
+                    mode: "grab",
+                  },
+                  resize: true,
+                },
+                modes: {
+                  bubble: {
+                    distance: 100,
+                    duration: 2,
+                    opacity: 0.8,
+                  },
+
+                  push: {
+                    quantity: 4,
+                  },
+                  repulse: {
+                    distance: 200,
+                    duration: 0.4,
+                  },
+                },
+              },
+
+              particles: {
+                size: { value: 5 },
+                color: {
+                  value: "#ffffff",
+                },
+
+                links: {
+                  enable: false,
+                },
+                collisions: {
+                  enable: true,
+                },
+
+                move: {
+                  direction: "up",
+                  enable: true,
+                  outMode: "bounce",
+                  random: false,
+                  speed: 2,
+                  fade: true,
+                  straight: false,
+                },
+                number: {
+                  value: 80,
+                },
+                opacity: {
+                  value: 0.7,
+                  random: {
+                    enable: true,
+                    minimumValue: 0.05,
+                  },
+                  animation: {
+                    enable: true,
+                    speed: 1,
+                    minimumValue: 0.05,
+                  },
+                },
+              },
+            }}
+          />
+        </div>
       </div>
     </div>
   );
